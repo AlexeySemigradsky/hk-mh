377445403577f7b16107ded42e02535fdf2e404b
Alexey Semigradsky <alexey.semigradsky@gmail.com>
2020-12-19 21:18:43 +0300

    many changes
    add config
    replace address with net.IP
    replace go-colorful with clr
    replace zap with log
    combine RGBService and WService into one

--- go.mod
+++ go.mod
@@ -3,10 +3,11 @@ module github.com/AlexeySemigradsky/hkmh
 go 1.15
 
 require (
-	github.com/AlexeySemigradsky/mh v1.0.0
+	github.com/AlexeySemigradsky/clr v1.0.0
+	github.com/AlexeySemigradsky/mh v1.2.0
 	github.com/brutella/hc v1.2.3
-	github.com/lucasb-eyer/go-colorful v1.0.3
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/kr/pretty v0.1.0 // indirect
 	github.com/xiam/to v0.0.0-20200126224905-d60d31e03561 // indirect
-	go.uber.org/multierr v1.6.0 // indirect
-	go.uber.org/zap v1.16.0
+	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
 )
--- service.go
+++ service.go
@@ -5,22 +5,17 @@ import (
 	"github.com/brutella/hc/service"
 )
 
-type RGBService struct {
+type Service struct {
 	*service.Service
-	Name       *characteristic.Name
 	On         *characteristic.On
 	Brightness *characteristic.Brightness
 	Saturation *characteristic.Saturation
 	Hue        *characteristic.Hue
 }
 
-func NewRGBService() *RGBService {
-	s := RGBService{}
+func NewService() *Service {
+	s := Service{}
 	s.Service = service.New(service.TypeLightbulb)
-
-	s.Name = characteristic.NewName()
-	s.Name.SetValue("RGB")
-	s.AddCharacteristic(s.Name.Characteristic)
 
 	s.On = characteristic.NewOn()
 	s.AddCharacteristic(s.On.Characteristic)
